1c6b91c8863855534dbf95e6bdc1b307d337ecd4
sukhpreet singh sodhi <ssinghsodhi@vmware.com>
2021-05-02 16:24:01 -0400

    Adding s

--- myproject/manage.py
+++ myproject/manage.py
@@ -10,7 +10,7 @@ import time
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    os.environ.setsdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
